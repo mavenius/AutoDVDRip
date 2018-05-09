69e684d6484b70594a18bcdb7d4ade01d74f40ff
Mark Avenius <mark@aveni.us>
2018-05-09 12:01:15 -0400

    Updates to support 2 drives.

--- UnregisterForDiscInsersion.ps1
+++ UnregisterForDiscInsersion.ps1
@@ -1,1 +1,11 @@
-Unregister-Event -SourceIdentifier discInserted
+Write-EventLog -LogName "Application" -Source "AutoDVDRip" -EventId 2 -EntryType Information -Message "Unregistering event."
+Try
+{
+    Unregister-Event -SourceIdentifier discInserted
+}
+Catch
+{
+    # todo: this is not logging on exception
+    Write-EventLog -LogName "Application" -Source "AutoDVDRip" -EventId 103 -EntryType Error -Message "Error encountered while unregistering event."
+    Break
+}
